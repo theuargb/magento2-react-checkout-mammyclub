d9c0289c74f45b7793401f3f4d2147b7d788da9d
Ростислав Сулейманов <rostilosfl@gmail.com>
2022-02-21 14:16:49 +0200

    add href to term-of-use page

--- src/reactapp/src/components/placeOrder/components/TermsInfo.jsx
+++ src/reactapp/src/components/placeOrder/components/TermsInfo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { __ } from '../../../i18n';
+import { config } from '../../../config';
 
 function TermsInfo() {
   return (
@@ -9,10 +10,7 @@ function TermsInfo() {
       </p>
       <p className="text-xs text-gray">
         {__('Вы соглашаетесь с')}
-        <a
-          className="text-link"
-          href="https://mammyclub.com/%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B5-%D1%81%D0%BE%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5"
-        >
+        <a className="text-link" href={`${config.baseUrl}/terms-of-use/`}>
           &nbsp;{__('пользовательским соглашением')}
         </a>
       </p>
