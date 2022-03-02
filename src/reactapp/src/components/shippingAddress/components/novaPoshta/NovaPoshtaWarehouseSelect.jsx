a6446c526b959f8974d33f85e8e81264c0b11e8b
Ростислав Сулейманов <rostilosfl@gmail.com>
2022-03-02 13:05:40 +0200

    virtualize novaPoshtaWarehouse list

--- src/reactapp/src/components/shippingAddress/components/novaPoshta/NovaPoshtaWarehouseSelect.jsx
+++ src/reactapp/src/components/shippingAddress/components/novaPoshta/NovaPoshtaWarehouseSelect.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { List, CellMeasurerCache, CellMeasurer } from 'react-virtualized';
 import { string, object } from 'prop-types';
-import Select, { components } from 'react-select';
+import Select, { components, createFilter } from 'react-select';
 import { formikDataShape } from '../../../../utils/propTypes';
 import { __ } from '../../../../i18n';
 import { config } from '../../../../config';
@@ -18,6 +19,7 @@ function NovaPoshtaWarehouseSelect({
   const [selectList, setSelectList] = useState(options);
   const { setFieldValue, setFieldTouched } = formikData;
   const [selectValue, setSelectValue] = useState('void');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFormChange = (e) => {
     const newValue = e.label;
@@ -28,6 +30,7 @@ function NovaPoshtaWarehouseSelect({
 
   useEffect(() => {
     setSelectValue('void');
+    setIsLoading(true);
     const postList = [];
     fetch(
       `${config.baseUrl}/rest/V1/novaposhta/warehouses?cityRef=${selectedCityId}`
@@ -49,18 +52,63 @@ function NovaPoshtaWarehouseSelect({
       .then(() => {
         if (postList.length > 0) {
           setSelectList(postList);
+          setIsLoading(false);
         } else {
           setSelectList(options);
+          setIsLoading(false);
         }
       });
   }, [selectedCityId]);
 
   /*eslint-disable*/
+  const cache = new CellMeasurerCache({
+    fixedWidth: true,
+    minHeight: 20,
+  });
 
   const CustomOption = ({ children, ...props }) => {
     const { onMouseMove, onMouseOver, ...rest } = props.innerProps;
     const newProps = { ...props, innerProps: rest };
     return <components.Option {...newProps}>{children}</components.Option>;
+  };
+
+  const CustomMenuList = (props) => {
+    const rows = props.children;
+    const rowRenderer = ({ key, index, style, parent, ...rest }) => {
+      return (
+        <CellMeasurer
+          cache={cache}
+          columnIndex={0}
+          key={key}
+          rowIndex={index}
+          parent={parent}
+        >
+          {({ measure, registerChild }) => (
+            <div ref={registerChild} key={key} style={style}>
+              <div onLoad={measure}>{rows[index]}</div>
+            </div>
+          )}
+        </CellMeasurer>
+      );
+    };
+
+    if(props.children.length){
+      return (
+        <List
+          style={{ width: '100%' }}
+          width={1000}
+          height={300}
+          rowHeight={cache.rowHeight}
+          rowCount={rows.length || 0}
+          rowRenderer={rowRenderer}
+        />
+      );
+    }else{
+      return(
+        <p class="text-sm py-2 px-3">{ __('No options')}</p>
+      )
+    }
+    
   };
 
   return (
@@ -72,6 +120,7 @@ function NovaPoshtaWarehouseSelect({
         options={selectList}
         placeholder=""
         onChange={(e) => handleFormChange(e)}
+        isLoading={isLoading}
         styles={{
           ...customStyles,
           option: (provided, { isDisabled }) => ({
@@ -86,7 +135,11 @@ function NovaPoshtaWarehouseSelect({
         }}
         value={selectValue}
         noOptionsMessage={() => __('No options')}
-        components={{ Option: CustomOption }}
+        components={{
+          Option: CustomOption,
+          MenuList: CustomMenuList,
+        }}
+        filterOption={createFilter({ ignoreAccents: false })}
       />
     </div>
   );
