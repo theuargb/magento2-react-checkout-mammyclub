f2de1d0215e5081b62c102eb04726455dfa4faa8
Ростислав Сулейманов <rostilosfl@gmail.com>
2022-07-06 16:12:08 +0300

    fixed remove from cart btn styles

--- src/reactapp/src/components/items/components/CartItem.jsx
+++ src/reactapp/src/components/items/components/CartItem.jsx
@@ -113,10 +113,11 @@ function CartItem({ item, isLastItem, actions }) {
             <div className="flex justify-end items-center pr-0.5 sm:pr-1">
               <div className="mr-0.5">{item.rowTotal}</div>
               <div className="h-full mt-1 mr-1 sm:mr-0 sm:mt-0 sm:pb-1 sm:static absolute top-0 right-0">
+                {/* eslint-disable */}
                 <button
                   onClick={handleRemoveProductClick}
                   name={qtyField}
-                  type="submit"
+                  type="button"
                   className="rounded-full text-xxs leading-0 text-white "
                   formikData={formikData}
                   style={{
@@ -124,28 +125,12 @@ function CartItem({ item, isLastItem, actions }) {
                     width: '12px',
                     height: '12px',
                     padding: '3px',
+                    backgroundSize: '6px 6px',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundImage: `url("data:image/svg+xml,%3Csvg version='1.1' id='Capa_1' stroke='white' fill='white' xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' viewBox='0 0 460.775 460.775' %3E%3Cpath d='M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z' /%3E%3C/svg%3E")`,
                   }}
-                >
-                  <svg
-                    version="1.1"
-                    id="Capa_1"
-                    stroke="white"
-                    fill="white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 460.775 460.775"
-                  >
-                    <path
-                      d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
-	c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
-	c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
-	c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
-	l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
-	c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"
-                    />
-                  </svg>
-                </button>
+                />
               </div>
             </div>
           )}
