dd76f617f65355bd26c83d079c5171732f8ce20c
Ростислав Сулейманов <rostilosfl@gmail.com>
2024-02-26 11:31:59 +0200

    stylefix, shipping cost fixes

--- src/reactapp/src/components/totals/Totals.jsx
+++ src/reactapp/src/components/totals/Totals.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 import { __ } from '../../i18n';
 import useTotalsCartContext from './hooks/useTotalsCartContext';
+import env from '../../utils/env';
+import RootElement from '../../utils/rootElement';
 
 function Totals() {
+  const storeCode = env.storeCode || RootElement.getStoreCode();
+  const altShippingCostTextStores = ['ua', 'ru'];
+  const zeroShippingCostText = altShippingCostTextStores.includes(storeCode)
+    ? __('at the carriers rates')
+    : 0;
+
   const {
     discounts,
     grandTotal,
@@ -11,18 +19,17 @@ function Totals() {
     hasShippingRate,
     shippingMethodRate,
   } = useTotalsCartContext();
-
   return (
     <div className="p-0 mt-0">
       <div>
         <div className="space-y-3">
-          <div className="px-4 py-3 flex justify-end text-xl font-bold text-gray">
-            <div className="text-lg text-gray">
-              {__('Delivery within Ukraine:\u00A0')}
-            </div>
-            <div className="text-lg text-green">
-              {(hasShippingRate && shippingMethodRate) || 0}
-            </div>
+          <div className="px-4 py-3 text-xl font-bold text-gray">
+            <span className="text-lg text-gray">
+              {__('Delivery cost:\u00A0')}
+            </span>
+            <span className="text-lg text-green">
+              {(hasShippingRate && shippingMethodRate) || zeroShippingCostText}
+            </span>
           </div>
 
           {hasDiscounts &&
