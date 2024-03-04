524a4377d5848fade749d5a94fc2936985b71b67
Ростислав Сулейманов <rostilosfl@gmail.com>
2024-03-04 16:08:09 +0200

    disable autocomplete on phone input (2)

--- src/reactapp/src/components/shippingAddress/components/ShippingAddressForm.jsx
+++ src/reactapp/src/components/shippingAddress/components/ShippingAddressForm.jsx
@@ -179,7 +179,7 @@ function ShippingAddressForm({ children }) {
           id="shipping_address.phone"
           required
           label={__('Phone (required)')}
-          autocomplete="off"
+          autocomplete="nope"
           name={fields.phone}
           formikData={formikData}
           onKeyDown={handleKeyDown}
